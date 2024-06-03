b7d2d9c6cb797aa3980dfa5c747f5e5c38bef966
Henrik Singmann <singmann@gmail.com>
2024-06-03 14:49:10 +0100

    added gumbel fit (v1)

--- fit_e1a.R
+++ fit_e1a.R
@@ -42,3 +42,48 @@ fe1a_uvsdt_1 <- brm(
   file = "fe1a_uvsdt_1", init_r = 0.5
 )
   
+### gumbel evsdt
+gumbel_formula <- brmsformula(
+  rating16 | vint(is_old)  ~ cond + (0 + cond|p|idx) +
+    (cond|stimID), 
+  crc ~ (1|p|idx), 
+  crlm ~ (1|p|idx), crll ~ (1|p|idx), 
+  crhm ~ (1|p|idx), crhh ~ (1|p|idx),
+  family = gumbel6p_family, cmc = FALSE
+)
+
+get_prior(gumbel_formula, data = e1a_reco)
+
+gumbel_priors <- prior(normal(0,0.5), class = Intercept, dpar = "crc") + 
+  prior(normal(-0.5,0.5), class = Intercept, dpar = "crlm") +
+  prior(normal(-0.5,0.5), class = Intercept, dpar = "crll") +
+  prior(normal(-0.5,0.5), class = Intercept, dpar = "crhm") +
+  prior(normal(-0.5,0.5), class = Intercept, dpar = "crhh") +
+  prior(student_t(3, 0, 2), class = b)
+
+stancode(gumbel_formula, data = e1a_reco, 
+         stanvars = sv_gumbel6p, 
+         prior = gumbel_priors)
+
+fe1a_gumbel_1 <- brm(
+  gumbel_formula, data = e1a_reco, 
+  stanvars = sv_gumbel6p, 
+  prior = gumbel_priors,
+  file = "fe1a_gumbel_1", init_r = 0.5
+)
+
+gumbel_formula2 <- brmsformula(
+  rating16 | vint(is_old)  ~ 0 + cond + (0 + cond|p|idx) +
+    (cond|stimID), 
+  crc ~ (1|p|idx), 
+  crlm ~ (1|p|idx), crll ~ (1|p|idx), 
+  crhm ~ (1|p|idx), crhh ~ (1|p|idx),
+  family = gumbel6p_family, cmc = FALSE
+)
+fe1a_gumbel_2 <- brm(
+  gumbel_formula2, data = e1a_reco, 
+  stanvars = sv_gumbel6p, 
+  prior = gumbel_priors,
+  file = "fe1a_gumbel_2", init_r = 0.5
+)
+  
---
+++ gumbel6p.R
@@ -0,0 +1,91 @@
+gumbel6p_stanvars <- "
+   real gumbelmin(real x, real mu, real disc){
+     //return 1- exp(-exp(-(-x-mu)/disc));
+     //return exp(gumbel_lccdf(-x | mu,disc));
+     return 1 - gumbel_cdf(-x|mu,disc);
+   }
+   real gumbel6p_lpmf(int y, real mu, 
+                   real crc, real crlm, real crll, real crhm, real crhh, 
+                   int itemtype) {
+     int nthres = 5;
+     real p;
+     real disc = 1;
+     vector[5] thres;
+     if (y == 1) {
+       thres[1] = crc - (exp(crlm) + exp(crll));
+       p = gumbelmin(thres[1], mu, disc);
+     } else if (y == nthres + 1) {
+     thres[nthres] = crc + (exp(crhm) + exp(crhh));
+       p = 1 - gumbelmin(thres[nthres], mu, disc);
+     } else {
+       if (y == 2) {
+         thres[1] = crc - (exp(crlm) + exp(crll));
+         thres[2] = crc - (exp(crlm));
+       } else if (y == 3) {
+         thres[2] = crc - (exp(crlm));
+         thres[3] = crc;
+       } else if (y == 4) {
+         thres[3] = crc;
+         thres[4] = crc + (exp(crhm));
+       } else if (y == 5) {
+         thres[4] = crc + (exp(crhm));
+         thres[5] = crc + (exp(crhm) + exp(crhh));
+       }
+       p = gumbelmin(thres[y], mu, disc) - gumbelmin(thres[y-1], mu, disc);
+     }
+     return log(p);
+   }
+"
+
+gumbel6p_family <- custom_family(
+  name = "gumbel6p", 
+  dpars = c("mu", "crc", "crlm", "crll", "crhm", "crhh"), 
+  links = c("identity", rep("identity", 5)), lb = c(NA, rep(NA, 5)),
+  type = "int", vars = "vint1[n]"
+)
+sv_gumbel6p <- stanvar(scode = uvsdt6p_stanvars, block = "functions")
+
+log_lik_gumbel6p <- function(i, prep) {
+  mu <- brms::get_dpar(prep, "mu", i = i)
+  discsignal <- brms::get_dpar(prep, "discsignal", i = i)
+  crc <- brms::get_dpar(prep, "crc", i = i)
+  crlm <- brms::get_dpar(prep, "crlm", i = i)
+  crll <- brms::get_dpar(prep, "crll", i = i)
+  crhm <- brms::get_dpar(prep, "crhm", i = i)
+  crhh <- brms::get_dpar(prep, "crhh", i = i)
+  
+  itemtype <- prep$data$vint1[i]
+  y <- prep$data$Y[i]
+  gumbel6p_lpmf(y, mu, crc, crlm, crll, crhm, crhh, itemtype)
+}
+posterior_predict_gumbel6p <- function(i, prep, ...) {
+  mu <- brms::get_dpar(prep, "mu", i = i)
+  #discsignal <- brms::get_dpar(prep, "discsignal", i = i)
+  crc <- brms::get_dpar(prep, "crc", i = i)
+  crlm <- brms::get_dpar(prep, "crlm", i = i)
+  crll <- brms::get_dpar(prep, "crll", i = i)
+  crhm <- brms::get_dpar(prep, "crhm", i = i)
+  crhh <- brms::get_dpar(prep, "crhh", i = i)
+  itemtype <- prep$data$vint1[i]
+  nsamples <- length(mu)
+  nthres <- 5
+  thres <- matrix(NA_real_, nrow = nsamples, ncol = nthres)
+  p <- matrix(NA_real_, nrow = nsamples, ncol = nthres+1) 
+  disc = rep(1, nsamples)
+  thres[,1] = crc - (exp(crlm) + exp(crll));
+  thres[,2] = crc - (exp(crlm));
+  thres[,3] = crc;
+  thres[,4] = crc + (exp(crhm));
+  thres[,5] = crc + (exp(crhm) + exp(crhh));
+  for (y in 1:(nthres+1)) {
+    if (y == 1) {
+      p[,y] = ordinal::pgumbel(thres[,1], mu, disc, max = FALSE)
+    } else if (y == nthres + 1) {
+      p[,y] = 1 - ordinal::pgumbel(thres[,nthres], mu, disc, max = FALSE)
+    } else {
+      p[,y] = ordinal::pgumbel(thres[,y], mu, disc, max = FALSE) -
+          ordinal::pgumbel(thres[,y-1], mu, disc, max = FALSE)
+    }
+  }
+  apply(p, 1, extraDistr::rcat, n = 1)
+}
